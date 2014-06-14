9dccd1e8198e56115e8268223f8d51bc4120c686
Andy <andy.cj.wu@gmail.com>
2014-06-13 23:25:05 -0400

    More solutions

--- Binary_Tree_Level_Order_Traversal_2.cpp
+++ Binary_Tree_Level_Order_Traversal_2.cpp
@@ -12,20 +12,21 @@ public:
     vector<vector<int>> levelOrderBottom(TreeNode *root) {
         if(root == nullptr) return vector<vector<int>>();
         
-        vector<vector<int>> result;
         queue<TreeNode *> p, q;
         p.push(root);
         
+        vector<vector<int>> result;
         vector<int> layer;
         TreeNode *t = nullptr;
+        
         while(!p.empty()){
             t = p.front();
-            
+            p.pop();
             layer.push_back(t->val);
+            
             if(t->left) q.push(t->left);
             if(t->right) q.push(t->right);
             
-            p.pop();
             if(p.empty()){
                 swap(p, q);
                 result.insert(result.begin(), layer);
